50021eaff60bcef9c742903509199837c4365eab
tmsb0606 <120014601+tmsb0606@users.noreply.github.com>
2024-03-06 14:35:10 +0900

    AddItemNumText
    アイテムを運んでいるキャラの数を表示するようにしました。

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Item : MonoBehaviour, ICarriable
 {
@@ -18,16 +19,34 @@ public class Item : MonoBehaviour, ICarriable
     public List<GameObject> carryObjects = new List<GameObject>();
     public int minCarryNum = 1;
     public int maxCarryNum = 1;
+    GameObject canvas;
+    TextMeshProUGUI text;
+
 
     void Start()
     {
+        canvas = Instantiate((GameObject)Resources.Load("Canvas"));
         
+        canvas.transform.parent = this.transform;
+        canvas.transform.localPosition = new Vector3(0, 0.5f, 0);
+
+        //canvas = gameObject.transform.Find("Canvas").gameObject;
+        text = canvas.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(carryObjects.Count > 0)
+        {
+            canvas.SetActive(true);
+            text.text = carryObjects.Count + "/" + minCarryNum;
+        }
+
+        if(canvas.active && carryObjects.Count == 0)
+        {
+            canvas.SetActive(false);
+        }
     }
     public void Carry(GameObject gameObject)
     {
